86f77e30a565aadf237e4acb604d59c628bc32d8
zenjutahi <zenjutahi@gmail.com>
2022-03-11 09:53:49 +0300

    rest webservice initial setup

---
+++ notes.md
@@ -0,0 +1,15 @@
+# RESTful Web Services
+
+
+eg. Social Media Application
+
+User -> Posts
+
+- Retrieve all Users 	- GET  /users
+- Create User 			- POST /users
+- Retrieve one User 	- GET  /users/{id} -> /users/1
+- Delete a User 		- DELETE /users/{id} -> /users/1
+
+- Retrieve all posts for a user  - GET /users/{id}/posts
+- Create a post for a user       - POST /users/{id}/posts
+- Retrieve details of a post     - GET /users/{id}/posts/{post_id}
---
+++ src/main/java/com/learning/rest/webservice/restfullwebservices/HelloWorldController.java
@@ -0,0 +1,26 @@
+package com.learning.rest.webservice.restfullwebservices;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+//Controller
+@RestController
+public class HelloWorldController {
+
+    //GET and URI - /hello-world
+    // method - that returns "Hello world"
+    /*
+    First way of mapping requests in Spring boot
+    @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+    public String helloWorld(){
+        return "Hello World";
+    }
+    */
+    //another way of mapping a request
+    @GetMapping(path = "/hello-world")
+    public String helloWorld(){
+        return "Hello World";
+    }
+}
