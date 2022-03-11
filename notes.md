c711c5c78ed8d922602801cdaa8202d9c5aa1c1a
zenjutahi <zenjutahi@gmail.com>
2022-03-11 10:31:32 +0300

    implementing a path variable mapping

--- notes.md
+++ notes.md
@@ -10,6 +10,13 @@ User -> Posts
 - Retrieve one User 	- GET  /users/{id} -> /users/1
 - Delete a User 		- DELETE /users/{id} -> /users/1
 
+
+
 - Retrieve all posts for a user  - GET /users/{id}/posts
 - Create a post for a user       - POST /users/{id}/posts
 - Retrieve details of a post     - GET /users/{id}/posts/{post_id}
+
+
+
+Spring Boot auto-configures most things like the dispatcher servlet
+and conversion of response to json
--- src/main/java/com/learning/rest/webservice/restfullwebservices/HelloWorldController.java
+++ src/main/java/com/learning/rest/webservice/restfullwebservices/HelloWorldController.java
@@ -1,9 +1,6 @@
 package com.learning.rest.webservice.restfullwebservices;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 //Controller
 @RestController
@@ -18,7 +15,7 @@ public class HelloWorldController {
         return "Hello World";
     }
     */
-    //another way of mapping a request
+    //another way of mapping requests
     @GetMapping(path = "/hello-world")
     public String helloWorld(){
         return "Hello World";
@@ -27,5 +24,11 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean(){
         return new HelloWorldBean("Hello World Today");
+    }
+
+    // simple resource mapping with path variable
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 }
