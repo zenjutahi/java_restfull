3101619402f3ec5c4d9a8a5e37d66bcb9a0f4b5e
zenjutahi <zenjutahi@gmail.com>
2022-03-12 17:16:01 +0300

    using h2 database and creating Entities

--- src/main/java/com/learning/rest/webservice/restfullwebservices/user/User.java
+++ src/main/java/com/learning/rest/webservice/restfullwebservices/user/User.java
@@ -1,11 +1,18 @@
 package com.learning.rest.webservice.restfullwebservices.user;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @Size(min=2, message = "name should have at least 2 characters")
@@ -13,6 +20,9 @@ public class User {
 
     @Past(message= "Ensure Birth date is in the Past")
     private Date birthDate;
+
+    public User() {
+    }
 
     public User(Integer id, String name, Date birthDate) {
         this.id = id;
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user values(1, sysdate(), 'mimi');
+insert into user values(2, sysdate(), 'wewe');
+insert into user values(3, sysdate(), 'yeye');
