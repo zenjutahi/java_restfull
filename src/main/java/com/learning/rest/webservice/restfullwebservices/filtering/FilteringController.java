c2b57011bf4aac041e1389579b137214aaa1babb
zenjutahi <zenjutahi@gmail.com>
2022-03-12 13:59:33 +0300

    static filtering using jsonignore

---
+++ src/main/java/com/learning/rest/webservice/restfullwebservices/filtering/FilteringController.java
@@ -0,0 +1,26 @@
+package com.learning.rest.webservice.restfullwebservices.filtering;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class FilteringController {
+
+
+
+    @GetMapping("/filtering")
+    public SomeBean retrievesSomeBean(){
+        return new SomeBean("Value1", "Value2", "Value3");
+    }
+
+    @GetMapping("/filtering/list")
+    public List<SomeBean> retrievesListOfSomeBean(){
+        return Arrays.asList(new SomeBean("Value1", "Value2", "Value3"),
+                new SomeBean("Value12", "Value22", "Value32"));
+    }
+
+}
---
+++ src/main/java/com/learning/rest/webservice/restfullwebservices/filtering/SomeBean.java
@@ -0,0 +1,42 @@
+package com.learning.rest.webservice.restfullwebservices.filtering;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class SomeBean {
+
+    private String field1;
+    private String field2;
+
+    @JsonIgnore
+    private String field3;
+
+    public SomeBean(String value1, String value2, String value3) {
+        this.field1 =value1;
+        this.field2 = value2;
+        this.field3 = value3;
+    }
+
+    public String getField1() {
+        return field1;
+    }
+
+    public String getField2() {
+        return field2;
+    }
+
+    public String getField3() {
+        return field3;
+    }
+
+    public void setField1(String field1) {
+        this.field1 = field1;
+    }
+
+    public void setField2(String field2) {
+        this.field2 = field2;
+    }
+
+    public void setField3(String field3) {
+        this.field3 = field3;
+    }
+}
